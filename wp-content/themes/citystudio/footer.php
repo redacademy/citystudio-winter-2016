ee3462137a0c2bd7b02bcf234d36c7006694228d
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-03-22 13:26:26 -0700

    Footer styles

--- wp-content/themes/citystudio/footer.php
+++ wp-content/themes/citystudio/footer.php
@@ -1,18 +1,32 @@
 <?php
 /**
  * The template for displaying the footer.
- *
  * @package RED_Starter_Theme
  */
-
 ?>
-
 			</div><!-- #content -->
-
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+				<div class="nav-div">
+				</div>
+				<div class="social-icons-div">
+					<ul>
+ 						<a href="https://www.facebook.com/CityStudioVancouver/">
+							<li>
+								<img src="<?php echo get_template_directory_uri().'/images/social-facebook-white.png';?>" class="social-icons" alt="Link to Citystudio Facebook Page" />
+							</li>
+						</a>
+						<a href="https://www.instagram.com/citystudiovan/?hl=en">
+							<li>
+								<img src="<?php echo get_template_directory_uri().'/images/social-instagram-white.png';?>" class="social-icons" alt="Link to Citystudio Instagram Feed" />
+							</li>
+						</a>
+						<a href="https://twitter.com/CityStudioVan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+							<li>
+								<img src="<?php echo get_template_directory_uri().'/images/social-twitter-white.png';?>" class="social-icons twitter" alt="Link to Citystudio Twitter Feed" />
+							</li>
+						</a>
+					</ul>
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
