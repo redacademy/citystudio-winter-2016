65aa1f08b7ab007198142e4511c29c5ea79608b9
Adam Dunlop <adamblairdunlop@gmail.com>
2016-03-27 13:11:06 -0700

    front-page loop implementation

--- wp-content/themes/citystudio/js/script.js
+++ wp-content/themes/citystudio/js/script.js
@@ -12,10 +12,8 @@ jQuery(document).ready(function($){
     autoControlsCombine: true,
     controls: false
   });
-});
 
-jQuery(document).ready(function(){
-  jQuery('.bxslider').bxSlider({
+  $('.bxslider').bxSlider({
     mode: 'horizontal',
     infiniteLoop: true,
     speed: 2000,
@@ -23,5 +21,42 @@ jQuery(document).ready(function(){
     auto: true,
     pager: false,
     controls: true
+  });
+
+  var gallery = ['featured-square', 'upper-mini', 'lower-regular'];
+  $('li').each(function(){
+    var i = $(this).index();
+    if( i === 1 ){
+      $(this).addClass(gallery[0]);
+    }
+    if( i === 2 ){
+      $(this).addClass(gallery[0]);
+    }
+    if( i === 3 ){
+      $(this).addClass(gallery[1]);
+    }
+    if( i === 4 ){
+      $(this).addClass(gallery[1]);
+    }
+    if( i === 5 ){
+      $(this).addClass(gallery[2]);
+    }
+    if( i === 6 ){
+      $(this).addClass(gallery[2]);
+    }
+    if( i === 7 ){
+      $(this).addClass(gallery[2]);
+    }
+    if( i === 8 ){
+      $(this).addClass(gallery[2]);
+    }
+    if( i === 9 ){
+      $(this).addClass(gallery[2]);
+    }
+    if( i === 10 ){
+      $(this).addClass(gallery[2]);
+    }
+
+
   });
 });
--- wp-content/themes/citystudio/template-parts/bxslider.php
+++ wp-content/themes/citystudio/template-parts/bxslider.php
@@ -1,9 +1,13 @@
 <ul class="bxslider">
-       
-        <?php query_posts(array('post_type' => 'project', 'posts_per_page' => 10)); if(have_posts()) : while(have_posts()) : the_post(); ?>
-        <?php $background = wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ), 'large' ); ?>
-        <li class="upper-mini" style="background: url('<?php echo $background; ?>') no-repeat;">
+		<?php query_posts(array('post_type' => 'project', 'posts_per_page' => 10));
+    	 if(have_posts()) : while(have_posts()) : the_post(); ?>
+    	<?php $background = wp_get_attachment_url( get_post_thumbnail_id( $page->ID[0] )); ?>
+
+        <li style="background: url('<?php echo $background; ?>') no-repeat">
         </li>
+  
+
        <?php endwhile; endif; wp_reset_query(); ?>
 
+	
 </ul>
