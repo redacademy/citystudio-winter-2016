ed850c7dcde282a886f9f9738195ba8f58ca628a
Adam Dunlop <adamblairdunlop@gmail.com>
2016-03-27 14:56:54 -0700

    anchor link functioning with some minor css

--- wp-content/themes/citystudio/js/script.js
+++ wp-content/themes/citystudio/js/script.js
@@ -23,40 +23,59 @@ $('#slider2').bxSlider({
     controls: true
   });
 
-  var gallery = ['featured-square', 'upper-mini', 'lower-regular'];
-  $('li').each(function(){
+  var gallery = [
+    'featured-square-1', 
+    'upper-mini-1', 
+    'upper-mini-2', 
+    'featured-square-2', 
+    'featured-square-3', 
+    'featured-square-4', 
+    'lower-regular-1', 
+    'lower-regular-2', 
+    'lower-regular-3', 
+    'lower-regular-4', 
+    'lower-regular-5'
+    ];
+  $('gallery-anchor').each(function(){
     var i = $(this).index();
-    if( i === 1 ){
+    if( i === 0 ){
       $(this).addClass(gallery[0]);
+    }
+    if( i === 1 ){
+      $(this).addClass(gallery[1]);
     }
     if( i === 2 ){
-      $(this).addClass(gallery[0]);
+      $(this).addClass(gallery[2]);
     }
     if( i === 3 ){
-      $(this).addClass(gallery[1]);
+      $(this).addClass(gallery[3]);
     }
     if( i === 4 ){
-      $(this).addClass(gallery[1]);
+      $(this).addClass(gallery[4]);
     }
     if( i === 5 ){
-      $(this).addClass(gallery[2]);
+      $(this).addClass(gallery[5]);
     }
     if( i === 6 ){
-      $(this).addClass(gallery[2]);
+      $(this).addClass(gallery[6]);
     }
     if( i === 7 ){
-      $(this).addClass(gallery[2]);
+      $(this).addClass(gallery[7]);
     }
     if( i === 8 ){
-      $(this).addClass(gallery[2]);
+      $(this).addClass(gallery[8]);
     }
     if( i === 9 ){
-      $(this).addClass(gallery[2]);
+      $(this).addClass(gallery[9]);
     }
     if( i === 10 ){
-      $(this).addClass(gallery[2]);
+      $(this).addClass(gallery[10]);
     }
 
-
   });
+
+  $('.gallery-anchor').hover(
+    function() {
+      $('.description').css({"opacity": "1"});
+    });
 });
--- wp-content/themes/citystudio/template-parts/bxslider.php
+++ wp-content/themes/citystudio/template-parts/bxslider.php
@@ -4,8 +4,13 @@
     	 if(have_posts()) : while(have_posts()) : the_post(); ?>
     	<?php $background = wp_get_attachment_url( get_post_thumbnail_id( $page->ID[0] )); ?>
 
-        <li style="background: url('<?php echo $background; ?>') no-repeat">
-        </li>
+        
+		<a class="gallery-anchor" href="<?php echo esc_url( get_permalink() ); ?>" >
+			<li style="background: url('<?php echo $background; ?>') no-repeat; "></li>
+
+			<div class="description"><span class="home-description><?php the_title( '<h2 class="description-title"> , </h2>' ); ?> <?php echo CFS()->get( 'subtitle' ); ?></span></div>
+
+        </a>
   
 
        <?php endwhile; endif; wp_reset_query(); ?>
