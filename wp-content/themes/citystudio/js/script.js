22bd84e73bad335b7b165f5ea1409279fab90648
emilie-nagahama <emilie.nagahama@gmail.com>
2016-03-31 13:52:01 -0700

    updating script.js

--- wp-content/themes/citystudio/js/script.js
+++ wp-content/themes/citystudio/js/script.js
@@ -1,107 +1,151 @@
+jQuery(document).ready(function($){
+​
+  $('.home-slider').flickity({
+    cellAlign: 'left',
+    contain: true,
+    initialIndex: 0,
+    accessibility: true,
+    wrapAround: true
+  });
+​
+  $('.main-carousel').flickity({
+    cellAlign: 'left',
+    contain: true,
+    initialIndex: 0,
+    accessibility: true,
+    prevNextButtons: false
+  });
+​
+  var gallery = [
+    'featured-square-1',
+    'upper-mini-1',
+    'upper-mini-2',
+    'featured-square-2',
+    'featured-square-3',
+    'featured-square-4',
+    'lower-regular-1',
+    'lower-regular-2',
+    'lower-regular-3',
+    'lower-regular-4',
+    'lower-regular-5',
+    'lower-regular-6'
+    ];
+​
+  $('.gallery-anchor').each(function(){
+    var i = $(this).index();
+    if( i === 0 ){
+      $(this).addClass(gallery[0]);
+    }
+    if( i === 1 ){
+      $(this).addClass(gallery[1]);
+    }
+    if( i === 2 ){
+      $(this).addClass(gallery[2]);
+    }
+    if( i === 3 ){
+      $(this).addClass(gallery[3]);
+    }
+    if( i === 4 ){
+      $(this).addClass(gallery[4]);
+    }
+    if( i === 5 ){
+      $(this).addClass(gallery[5]);
+    }
+    if( i === 6 ){
+      $(this).addClass(gallery[6]);
+    }
+    if( i === 7 ){
+      $(this).addClass(gallery[7]);
+    }
+    if( i === 8 ){
+      $(this).addClass(gallery[8]);
+    }
+    if( i === 9 ){
+      $(this).addClass(gallery[9]);
+    }
+    if( i === 10 ){
+      $(this).addClass(gallery[10]);
+    }
+​
+  });
+});
+​
+/* Jquery for Footer Navigation -- should go in own file soon */
+​
 jQuery(document).ready(function($) {
-
-    $('.main-carousel').flickity({
-        cellAlign: 'left',
-        contain: true,
-        initialIndex: 0,
-        accessibility: true,
-        prevNextButtons: false
-    });
-
-
-    var gallery = ['featured-square-1', 'upper-mini-1', 'upper-mini-2', 'featured-square-2', 'featured-square-3', 'featured-square-4', 'lower-regular-1', 'lower-regular-2', 'lower-regular-3', 'lower-regular-4', 'lower-regular-5'];
-
-    $('.gallery-anchor').each(function() {
-        var i = $(this).index();
-
-        if (i === 0) {
-            $(this).addClass(gallery[0]);
-        }
-        if (i === 1) {
-            $(this).addClass('flex');
-            $(this).addClass(gallery[1]);
-        }
-        if (i === 2) {
-            $(this).addClass('flex');
-            $(this).addClass(gallery[2]);
-        }
-        if (i === 3) {
-            $(this).addClass(gallery[3]);
-        }
-        if (i === 4) {
-            $(this).addClass(gallery[4]);
-        }
-        if (i === 5) {
-            $(this).addClass(gallery[5]);
-        }
-        if (i === 6) {
-            $(this).addClass(gallery[6]);
-        }
-        if (i === 7) {
-            $(this).addClass(gallery[7]);
-        }
-        if (i === 8) {
-            $(this).addClass(gallery[8]);
-        }
-        if (i === 9) {
-            $(this).addClass(gallery[9]);
-        }
-        if (i === 10) {
-            $(this).addClass(gallery[10]);
-        }
-    }); //close each funtciont
-
-    var queryFilter = {
-        checkedNeigh: '',
-        checkedPart: '',
-        checkedYear: ''
-    };
-
+​
+    var checkedNeigh = '';
+    var checkedPart = '';
+​
     $('.foot-sub-menu label').hide();
     $('.neigh-labels').hide();
     $('.part-labels').hide();
-    $('.year-labels').hide();
-
-    $('.menu-item').hover(function() {
-        $(this).children('.foot-sub-menu').toggleClass('current-menu');
+​
+    $('.menu-item').hover(function(){
+      	  $(this).children('.foot-sub-menu').toggleClass('current-menu');
     });
-
+​
     $('.sub-menu-neigh').click(function() {
-        if ($(this.checked)) {
-            neighValue = $(this).find('input').val();
-            queryFilter.checkedNeigh = ($(this).text()).trim();
-            $('.neigh-labels')
-                .show()
-                .append('<label>' + queryFilter.checkedNeigh + '</label>');
-            $(this).parent().hide();
-            reloadProjects();
-        }
+        if($(this.checked)){
+           checkedNeigh += ($(this).text());
+           $('.neigh-labels').show().append('<label>'+checkedNeigh+'</label>');
+           $('.foot-sub-menu').hide();
+      }
     });
+​
     $('.sub-menu-part').click(function() {
-        if ($(this.checked)) {
-            partValue = $(this).find('input').val();
-            queryFilter.checkedPart = ($(this).text()).trim();
-            $('.part-labels')
-                .show()
-                .append('<label>' + queryFilter.checkedPart + '</label>');
-            $(this).parent().hide();
-            reloadProjects();
-        }
+        if($(this.checked)){
+           checkedPart += ($(this).text());
+           $('.part-labels').show().append('<label>'+checkedPart+'</label>');
+           $('.foot-sub-menu').hide();
+      }
     });
-    $('.sub-menu-year').click(function() {
-        if ($(this.checked)) {
-            yearValue = $(this).find('input').val();
-            queryFilter.checkedYear = ($(this).text()).trim();
-            $('.year-labels')
-                .show()
-                .append('<label>' + queryFilter.checkedYear + '</label>');
-            $(this).parent().hide();
-            reloadProjects();
-        } // close if
-
+​
+​
+    // $('.sub-menu-item span').click(function(){
+    //     $('.foot-sub-menu').hide();
+    //     $('').show();
+    // });
+​
+​
+    /* Jquery for header and footer display*/
+​
+  $('.sub-menu-neigh').click(function() {
+    if ($(this.checked)) {
+      neighValue = $(this).find('input').val();
+      queryFilter.checkedNeigh = ($(this).text()).trim();
+      $('.neigh-labels')
+      .show()
+      .append('<label>' + queryFilter.checkedNeigh + '</label>');
+      $(this).parent().hide();
+      reloadProjects();
+    }
+  });
+  $('.sub-menu-part').click(function() {
+    if ($(this.checked)) {
+      partValue = $(this).find('input').val();
+      queryFilter.checkedPart = ($(this).text()).trim();
+      $('.part-labels')
+      .show()
+      .append('<label>' + queryFilter.checkedPart + '</label>');
+      $(this).parent().hide();
+      reloadProjects();
+    }
+  });
+  $('.sub-menu-year').click(function() {
+    if ($(this.checked)) {
+      yearValue = $(this).find('input').val();
+      queryFilter.checkedYear = ($(this).text()).trim();
+      $('.year-labels')
+      .show()
+      .append('<label>' + queryFilter.checkedYear + '</label>');
+      $(this).parent().hide();
+      reloadProjects();
+      } // close if
     }); // close sub-menu-year
-
+​
     function reloadProjects() {
+​
         $.ajax({
           type: 'GET',
           dataType: 'json',
@@ -116,4 +160,5 @@ jQuery(document).ready(function($) {
               //relaoad the UI
           }); // close ajax call
   } // close reload projects})
+​
 });
