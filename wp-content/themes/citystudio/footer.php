71e831615a419364ed95d6e916162d5f4ef67587
emilie-nagahama <emilie.nagahama@gmail.com>
2016-03-31 17:07:00 -0700

    add loop to dynamically populate the navigation

--- wp-content/themes/citystudio/footer.php
+++ wp-content/themes/citystudio/footer.php
@@ -11,68 +11,66 @@
 						<div class="sort">
 							<span>Sort</span>
 						</div>
+
 						<div class="neighbourhood menu-item" id="neighbourhood">
-							<ul class="foot-sub-menu neigh">
-								<li><span class="menu-title">Neighbourhood</span></li>
-								<li class="sub-menu-neigh" id="kitsilano">
-									<input type="checkbox" name="neighbourhood" value="kitsilano" />
-									<span>Kitsilano</span>
-								</li>
-								<li class="sub-menu-neigh" id="pleasant">
-									<input type="checkbox" name="neighbourhood" value="mt-pleasant" />
-									<span>Mt. Pleasant</span>
-								</li>
-								<li class="sub-menu-neigh" id="commercial">
-									<input type="checkbox" name="neighbourhood" value="commercial-dr" />
-									<span>Commercial Dr.</span>
-								</li>
-							</ul>
-							<div class="neigh-labels">
-							</div>
+							<?php $terms = get_terms( 'neighbourhoods', array (
+								'hide_empty' => 0,
+							) ); ?>
+						  <?php if ( ! empty( $terms ) ) : ?>
+								<ul class="foot-sub-menu neigh">
+									<li><span class="menu-title">Neighbourhoods</span></li>
+								<?php foreach( $terms as $term) : ?>
+									<li class="sub-menu-neigh">
+										<input type="checkbox" name="<?php echo $term->name ?>" value="<?php echo $term->slug ?>" />
+										<span><?php echo $term->name ?></span>
+									</li>
+						    <?php endforeach; ?>
+								</ul>
+						  <?php endif; ?>
+							<div class="neigh-labels"></div>
 						</div>
 
 						<div class="partners menu-item">
-							<ul class="foot-sub-menu part">
-								<li><span class="menu-title">Partners</span></li>
-								<li class="sub-menu-part" id="SFU">
-									<input type="checkbox" name="partners" value="sfu" />
-									<span>SFU</span>
-								</li>
-								<li class="sub-menu-part" id="ubc">
-									<input type="checkbox" name="partners" value="ubc" />
-									<span>UBC</span>
-								</li>
-								<li class="sub-menu-part" id="bcit">
-									<input type="checkbox" name="partners" value="bcit" />
-									<span>BCIT</label>
-								</li>
+							<?php $terms = get_terms( 'partners', array (
+								'hide_empty' => 0,
+							) ); ?>
+							<?php if ( ! empty( $terms ) ) : ?>
+								<ul class="foot-sub-menu part">
+									<li><span class="menu-title">Partners</span></li>
+									<?php foreach( $terms as $term) : ?>
+									<li class="sub-menu-part">
+										<input type="checkbox" name="<?php echo $term->name ?>" value="<?php echo $term->slug ?>" />
+										<span><?php echo $term->name ?></span>
+									</li>
+							<?php endforeach; ?>
 							</ul>
-							<div class="part-labels">
-							</div>
+							<?php endif; ?>
+							<div class="part-labels"></div>
 						</div>
 
 						<div class="years menu-item">
+							<?php $terms = get_terms( 'year', array (
+								'hide_empty' => 0,
+							) ); ?>
+							<?php if ( ! empty( $terms ) ) : ?>
 							<ul class="foot-sub-menu year">
 								<li><span class="menu-title">Year</span></li>
-								<li class="sub-menu-year" id="2014">
-									<input type="checkbox" name="year" value="2014" />
-									<span>2014</span>
-								</li>
-								<li class="sub-menu-year" id="2015">
-									<input type="checkbox" name="year" value="2015" />
-									<span>2015</span>
-								</li>
-								<li class="sub-menu-year" id="2016">
-									<input type="checkbox" name="year" value="2016" />
-									<span>2016</label>
-								</li>
+									<?php foreach( $terms as $term) : ?>
+									<li class="sub-menu-year">
+										<input type="checkbox" name="<?php echo $term->name ?>" value="<?php echo $term->slug ?>" />
+										<span><?php echo $term->name ?></span>
+									</li>
+							<?php endforeach; ?>
 							</ul>
+							<?php endif; ?>
 							<div class="year-labels">
 							</div>
 						</div>
+
 						<div class="refresh menu-item" id="refresh">
 							<span></span>
 						</div>
+						
 					</div>
 				</div>
 				<div class="social-icons-div">
