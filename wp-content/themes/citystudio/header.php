cecf5b57bcb01c752aedb32c6ebb76e3d8f8bd67
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-04-04 14:59:08 -0700

    css changes

--- wp-content/themes/citystudio/header.php
+++ wp-content/themes/citystudio/header.php
@@ -15,7 +15,16 @@
 
 	<?php wp_head(); ?>
 
-
+	<style media="screen and (max-width: 782px)">
+			html {
+				margin-top:0 !important;
+			}
+	</style>
+	<style media="screen">
+			html {
+				margin-top:0 !important;
+			}
+	</style>
 	</head>
 
 	<body <?php body_class(); ?>>
@@ -26,7 +35,7 @@
 			<div class="header-container">
 
 			<div class="site-branding">
-				<a href="<?php echo esc_url(home_url());?>">
+				<a href="<?php echo esc_url(home_url([1]));?>">
 					<img src="<?php bloginfo('template_directory'); ?>/images/citystudio-logo.png" class="logo" alt="Citystudio logo">
 				</a>
 			</div><!-- .site-branding -->
