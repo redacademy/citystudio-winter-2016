fb68207ac2685551c0315a62408dbe49551cdd0b
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-04-03 17:35:03 -0700

    add search page

--- wp-content/themes/citystudio/header.php
+++ wp-content/themes/citystudio/header.php
@@ -33,7 +33,7 @@
 			<div class="search-gallery">
 			<div class="hide-search">
 				<span class="icon-search" aria-hidden="true">
-					<a href="<?php echo esc_url(home_url('/search'));?>"><i class="fa fa-search"></i></a>
+					<a href="<?php echo esc_url(home_url('/search-page?s='));?>"><i class="fa fa-search"></i></a>
 				</span>
 			</div>
 				<div class="icon-gallery">
--- wp-content/themes/citystudio/search.php
+++ wp-content/themes/citystudio/search.php
@@ -6,31 +6,33 @@
  */
 
 get_header(); ?>
-
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="searchpage">
+			<br>
 
+					<?php get_search_form(); ?>
+
+					<div class="search-list">
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+				<a href="<?php echo the_permalink($project);?>">
+		      <?php if (has_post_thumbnail()) : ?>
+			      <?php the_post_thumbnail('medium', 'style=width:400px;height:300px;margin:.5rem;'); ?>
+			    <?php endif; ?>
+				</a>
 
 			<?php endwhile; ?>
 
-			<?php red_starter_numbered_pagination(); ?>
-
 		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
 
+			</div>
+			</div>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
