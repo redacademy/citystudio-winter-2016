ad9dd391f4f4a7384eb404340355348da28b7610
Adam Dunlop <adamblairdunlop@gmail.com>
2016-05-11 12:06:45 -0700

    fixing more indentation and refactoring code

--- wp-content/themes/citystudio/footer.php
+++ wp-content/themes/citystudio/footer.php
@@ -12,24 +12,24 @@
 
 				<div class="social-icons-div">
  					<p>
-							<a href="https://www.facebook.com/CityStudioVancouver/">
-								<img src="<?php echo get_template_directory_uri().'/images/social-facebook-white.png';?>" class="social-icons" alt="Link to Citystudio Facebook Page" />
-							</a>
+						<a href="https://www.facebook.com/CityStudioVancouver/">
+							<img src="<?php echo get_template_directory_uri().'/images/social-facebook-white.png';?>" class="social-icons" alt="Link to Citystudio Facebook Page" />
+						</a>
 					</p>
 					<p>
-												<a href="https://www.instagram.com/citystudiovan/?hl=en">
-								<img src="<?php echo get_template_directory_uri().'/images/social-instagram-white.png';?>" class="social-icons" alt="Link to Citystudio Instagram Feed" />
-							</a>
+						<a href="https://www.instagram.com/citystudiovan/?hl=en">
+							<img src="<?php echo get_template_directory_uri().'/images/social-instagram-white.png';?>" class="social-icons" alt="Link to Citystudio Instagram Feed" />
+						</a>
 					</p>
 					<p>
-							<a href="https://twitter.com/CityStudioVan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
-								<img src="<?php echo get_template_directory_uri().'/images/social-twitter-white.png';?>" class="social-icons twitter" alt="Link to Citystudio Twitter Feed" />
-							</a>
+						<a href="https://twitter.com/CityStudioVan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+							<img src="<?php echo get_template_directory_uri().'/images/social-twitter-white.png';?>" class="social-icons twitter" alt="Link to Citystudio Twitter Feed" />
+						</a>
 					</p>
 					<p>
-							<a href="http://www.youtube.com/user/CityStudioVancouver">
-								<img src="<?php echo get_template_directory_uri().'/images/social-youtube-white.png';?>" class="social-icons twitter" alt="Link to Citystudio Twitter Feed" />
-							</a>
+						<a href="http://www.youtube.com/user/CityStudioVancouver">
+							<img src="<?php echo get_template_directory_uri().'/images/social-youtube-white.png';?>" class="social-icons twitter" alt="Link to Citystudio Twitter Feed" />
+						</a>
 					</p>
 				</div>
 			</footer><!-- #colophon -->
