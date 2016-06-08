67e5c33c81a3e298ff3af0ce898d9fc64f9dfbea
Adam Dunlop <adamblairdunlop@gmail.com>
2016-06-08 16:48:35 -0700

    footer css touch up

--- wp-content/themes/citystudio/footer.php
+++ wp-content/themes/citystudio/footer.php
@@ -14,21 +14,15 @@
 							<a href="https://www.facebook.com/CityStudioVancouver/">
 								<img src="<?php echo get_template_directory_uri().'/images/social-facebook-white.png';?>" class="social-icons" alt="Link to Citystudio Facebook Page" />
 							</a>
-						<p>
 							<a href="https://www.instagram.com/citystudiovan/?hl=en">
 								<img src="<?php echo get_template_directory_uri().'/images/social-instagram-white.png';?>" class="social-icons" alt="Link to Citystudio Instagram Feed" />
 							</a>
-						</p>
-						<p>
 							<a href="https://twitter.com/CityStudioVan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
 								<img src="<?php echo get_template_directory_uri().'/images/social-twitter-white.png';?>" class="social-icons twitter" alt="Link to Citystudio Twitter Feed" />
 							</a>
-						</p>
-						<p>
 							<a href="http://www.youtube.com/user/CityStudioVancouver">
 								<img src="<?php echo get_template_directory_uri().'/images/social-youtube-white.png';?>" class="social-icons twitter" alt="Link to Citystudio YouTube Feed" />
 							</a>
-						</p>
 					</div>
 				</div>	
 				<div class="footer-located">
