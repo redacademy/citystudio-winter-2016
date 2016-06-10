d64c1e9e7690deb86cf1ff5647c39042927a95dc
Adam Dunlop <adamblairdunlop@gmail.com>
2016-06-10 16:35:05 -0700

    added copyrite under the footer

--- wp-content/themes/citystudio/footer.php
+++ wp-content/themes/citystudio/footer.php
@@ -5,60 +5,64 @@
  */
 ?>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+		<footer id="colophon" class="site-footer" role="contentinfo">
 
-		<div class="footer-branding-container">
-			<div class="footer-logo">
-				<img src="<?php echo get_template_directory_uri().'/images/citystudio-logo.png';?>">
-			</div>
-			<div class="social-icons-div">
-							<a href="https://www.facebook.com/CityStudioVancouver/">
-								<img src="<?php echo get_template_directory_uri().'/images/social-facebook-white.png';?>" class="social-icons" alt="Link to Citystudio Facebook Page" />
-							</a>
-							<a href="https://www.instagram.com/citystudiovan/?hl=en">
-								<img src="<?php echo get_template_directory_uri().'/images/social-instagram-white.png';?>" class="social-icons" alt="Link to Citystudio Instagram Feed" />
-							</a>
-							<a href="https://twitter.com/CityStudioVan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
-								<img src="<?php echo get_template_directory_uri().'/images/social-twitter-white.png';?>" class="social-icons" alt="Link to Citystudio Twitter Feed" />
-							</a>
-							<a href="http://www.youtube.com/user/CityStudioVancouver">
-								<img src="<?php echo get_template_directory_uri().'/images/social-youtube-white.png';?>" class="social-icons" alt="Link to Citystudio YouTube Feed" />
-							</a>
-			</div>
-		</div> <!--end footer branding -->
-
-		<div class="footer-info-container">
-				<div class="footer-info-block">
-					<h3>Located At</h3>
-					<ul>
-						<li>1800 Spyglass Place</li>
-						<li>Vancouver, B.C.</li>
-						<li>604-874-6401</li>
-					</ul>
+			<div class="footer-branding-container">
+				<div class="footer-logo">
+					<img src="<?php echo get_template_directory_uri().'/images/citystudio-logo.png';?>">
 				</div>
-
-				<div class="footer-info-block">
-					<h3>Keep Exploring</h3>
-					<ul>
-						<li>Projects</li>
-						<li>Campus Courses</li>
-						<li>For Students</li>
-						<li>Partners</li>
-					</ul>
+				<div class="social-icons-div">
+								<a href="https://www.facebook.com/CityStudioVancouver/">
+									<img src="<?php echo get_template_directory_uri().'/images/social-facebook-white.png';?>" class="social-icons" alt="Link to Citystudio Facebook Page" />
+								</a>
+								<a href="https://www.instagram.com/citystudiovan/?hl=en">
+									<img src="<?php echo get_template_directory_uri().'/images/social-instagram-white.png';?>" class="social-icons" alt="Link to Citystudio Instagram Feed" />
+								</a>
+								<a href="https://twitter.com/CityStudioVan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+									<img src="<?php echo get_template_directory_uri().'/images/social-twitter-white.png';?>" class="social-icons" alt="Link to Citystudio Twitter Feed" />
+								</a>
+								<a href="http://www.youtube.com/user/CityStudioVancouver">
+									<img src="<?php echo get_template_directory_uri().'/images/social-youtube-white.png';?>" class="social-icons" alt="Link to Citystudio YouTube Feed" />
+								</a>
 				</div>
+			</div> <!--end footer branding -->
 
-				<div class="footer-info-block">
-					<h3>CITYSTUDIO</h3>
-					<ul>
-						<li>Contact Us</li>
-						<li>People</li>
-						<li>Consulting</li>
-						<li>Blog</li>
-					</ul>
-				</div>
-		</div><!-- end footer info container -->
-			</footer><!-- #colophon -->
-		</div><!-- #content -->
+			<div class="footer-info-container">
+					<div class="footer-info-block">
+						<h3>Located At</h3>
+						<ul>
+							<li>1800 Spyglass Place</li>
+							<li>Vancouver, B.C.</li>
+							<li>604-874-6401</li>
+						</ul>
+					</div>
+
+					<div class="footer-info-block">
+						<h3>Keep Exploring</h3>
+						<ul>
+							<li>Projects</li>
+							<li>Campus Courses</li>
+							<li>For Students</li>
+							<li>Partners</li>
+						</ul>
+
+					</div>
+
+					<div class="footer-info-block">
+						<h3>CITYSTUDIO</h3>
+						<ul>
+							<li>Contact Us</li>
+							<li>People</li>
+							<li>Consulting</li>
+							<li>Blog</li>
+						</ul>
+					</div>
+			</div><!-- end footer info container -->
+
+			
+		</footer><!-- #colophon -->
+		<div class="copyrite">© Copyrite CityStudio Vancouver</div>
+	</div><!-- #content -->
 		<?php wp_footer(); ?>
 	</body>
 </html>
