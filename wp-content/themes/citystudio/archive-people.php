cc9c9da591276b1e798704c393349ea1979b40cc
Adam Dunlop <adamblairdunlop@gmail.com>
2016-06-15 08:25:35 -0700

    fixed css issues on homepage

--- wp-content/themes/citystudio/archive-people.php
+++ wp-content/themes/citystudio/archive-people.php
@@ -7,26 +7,26 @@ get_header();
 
 <div id="primary" class="content-area single-projects">
 	<main id="main" class="site-main" role="main">
+    <div class="people-container">
+      <div class="people-archive">
 
-    <div class="people-archive">
+        <header class="citystudio-banner">
+          <div class="banner-inner">
+            <h2 class="page-titles">People</h2>
+          </div>
+        </header>
 
-      <header class="citystudio-banner">
-        <div class="banner-inner">
-          <h2 class="page-titles">People</h2>
-        </div>
-      </header>
+      <?php get_template_part( 'template-parts/people', 'team' ); ?>
 
-    <?php get_template_part( 'template-parts/people', 'team' ); ?>
+      <?php get_template_part( 'template-parts/people', 'leadership' ); ?>
 
-    <?php get_template_part( 'template-parts/people', 'leadership' ); ?>
+      <?php get_template_part( 'template-parts/people', 'operations' ); ?>
 
-    <?php get_template_part( 'template-parts/people', 'operations' ); ?>
-
-    <?php get_template_part( 'template-parts/people', 'founders' ); ?>
+      <?php get_template_part( 'template-parts/people', 'founders' ); ?>
 
 
-    </div><!-- .people-archive -->
-
+      </div><!-- .people-archive -->
+    </div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
