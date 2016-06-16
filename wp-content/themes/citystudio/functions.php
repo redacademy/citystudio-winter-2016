3027aa473c8e6d7377e6376fccd38951d48c6ed9
emilie-nagahama <emilie.nagahama@gmail.com>
2016-06-15 18:46:45 -0700

    load tags on single project and cleaned up tags output a little

--- wp-content/themes/citystudio/functions.php
+++ wp-content/themes/citystudio/functions.php
@@ -171,6 +171,19 @@ function get_featured_project( $object, $field_name, $request ) {
 		return get_post_meta( $object[ 'id' ], $field_name );
 }
 
+// add tag support to pages
+function tags_support_all() {
+	register_taxonomy_for_object_type('post_tag', 'project');
+}
+
+// ensure all tags are included in queries
+function tags_support_query($wp_query) {
+	if ($wp_query->get('tag')) $wp_query->set('post_type', 'any');
+}
+
+// tag hooks
+add_action('init', 'tags_support_all');
+add_action('pre_get_posts', 'tags_support_query');
 
 /**
  * Custom template tags for this theme.
