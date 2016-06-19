9ab7dd9c9d9f0cd5857a96185f47d2b13d19205c
emilie-nagahama <emilie.nagahama@gmail.com>
2016-06-19 15:27:01 -0700

    fix custom taxonomy page issues

--- wp-content/themes/citystudio/taxonomy.php
+++ wp-content/themes/citystudio/taxonomy.php
@@ -1,45 +1,39 @@
 <?php
 /**
- * The template for displaying archive pages.
- *
- * @package RED_Starter_Theme
+ * Template Name: Tag Archives
  */
+ get_header(); ?>
 
-get_header(); ?>
+<div id="primary" class="content-area tag-archives">
+  <main id="main" class="site-main" role="main">
 
-	<h1> The archive page </h1>
+  <?php if ( have_posts() ) : ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <header class="page-header archive-header">
+      <?php
+            echo single_term_title('<h2><span>Tags:</span> ', '</h2>' );
+      ?>
+    </header><!-- .page-header -->
 
-		<?php if ( have_posts() ) : ?>
+      <div class="section-archive">
+        <?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+        <ul>
+          <?php $background = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() )); ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+          <a href="<?php echo esc_url( get_permalink() ); ?>"><li style="background: url('<?php echo $background; ?>') no-repeat; ">
+          </li></a>
+        </ul>
+        <?php endwhile; ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+        <?php else : ?>
 
+        <?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-				<?php
-					get_template_part( 'template-parts/archive-tag' );
-				?>
+        <?php endif; ?>
+      </div>
 
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
