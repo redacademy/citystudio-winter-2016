01ff85b3e631830d369aff4d19d403c4c1925331
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-06-19 12:24:55 -0700

    partner and single page final touch ups

--- wp-content/plugins/citystudio-custom-funcs/lib/functions/post-types.php
+++ wp-content/plugins/citystudio-custom-funcs/lib/functions/post-types.php
@@ -6,7 +6,7 @@
 
 // Custom Post Types for City Studio Projects
 
-// Register Custom Post Type
+// Register Project Custom Post Type
 function register_project_post_type() {
 
 	$labels = array(
@@ -69,7 +69,7 @@ function register_project_post_type() {
 add_action( 'init', 'register_project_post_type', 0 );
 
 
-// Register Custom Post Type
+// Register People Custom Post Type
 function register_people_post_type() {
 
 	$labels = array(
@@ -123,3 +123,58 @@ function register_people_post_type() {
 
 }
 add_action( 'init', 'register_people_post_type', 0 );
+
+// Register Partners Custom Post Type
+function register_partners_post_type()  {
+
+	$labels = array(
+		'name'                  => _x( 'Partner', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Partner', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Partners', 'text_domain' ),
+		'name_admin_bar'        => __( 'Partner', 'text_domain' ),
+		'archives'              => __( 'Partner Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Partner Item:', 'text_domain' ),
+		'all_items'             => __( 'All Partners', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Partner', 'text_domain' ),
+		'add_new'               => __( 'Add New Partner', 'text_domain' ),
+		'new_item'              => __( 'New Partner', 'text_domain' ),
+		'edit_item'             => __( 'Edit Partner', 'text_domain' ),
+		'update_item'           => __( 'Update Partner', 'text_domain' ),
+		'view_item'             => __( 'View Partner', 'text_domain' ),
+		'search_items'          => __( 'Search Partners', 'text_domain' ),
+		'not_found'             => __( 'Partner not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Partner Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into partner', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this partner', 'text_domain' ),
+		'items_list'            => __( 'Partners list', 'text_domain' ),
+		'items_list_navigation' => __( 'Partners list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter partners list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Partner', 'text_domain' ),
+		'description'           => __( 'Participating Schools', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-welcome-learn-more',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'Partners', $args );
+
+}
+add_action( 'init', 'register_partners_post_type', 0 );
