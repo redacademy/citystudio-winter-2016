b2c3bb6f9013b046970af6e0df8876b7cbb5b917
emilie-nagahama <emilie.nagahama@gmail.com>
2016-06-23 19:06:45 -0700

    tag.template for custom tags

--- wp-content/themes/citystudio/tag.php
+++ wp-content/themes/citystudio/tag.php
@@ -1,28 +1,26 @@
 <?php
 /**
- * 
+ * Template Name: Taxonomy Archives
  */
  get_header(); ?>
 
 <div id="primary" class="content-area tag-archives">
   <main id="main" class="site-main" role="main">
 
-  <?php if ( have_posts() ) : ?>
-
     <header class="page-header archive-header">
-      <p>Tag: <?php single_tag_title(); ?></p>
-      <?php
-            echo single_term_title('<h2><span>Tags:</span> ', '</h2>' );
-      ?>
+
+      <h2><span>Tag:</span> <?php single_tag_title(); ?></h2>
+
     </header><!-- .page-header -->
+
+  <?php if ( have_posts() ) : ?>
 
       <div class="section-archive">
         <?php /* Start the Loop */ ?>
         <?php while ( have_posts() ) : the_post(); ?>
         <ul>
-          <?php $background = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() )); ?>
-
-          <a href="<?php echo esc_url( get_permalink() ); ?>"><li style="background: url('<?php echo $background; ?>') no-repeat; ">
+          <li>
+            <a href="<?php echo esc_url( get_permalink() ); ?>"><li style="background: url('<?php the_field('project_featured_image'); ?>') no-repeat; ">
           </li></a>
         </ul>
         <?php endwhile; ?>
