7ad3fdc49e10ce78979fe6c17693823c8e105492
Adam Dunlop <adamblairdunlop@gmail.com>
2016-09-22 21:56:18 -0700

    updated mobile menu

--- wp-content/themes/citystudio/template-parts/page-home/intro.php
+++ wp-content/themes/citystudio/template-parts/page-home/intro.php
@@ -1,5 +1,10 @@
 
   <h1 class="homepage-titles"><span>Explore</span> CityStudio Projects</h1>
   <p class="homepage-description">
-    We partner with local post-secondary schools to spur student-driven projects that make Vancouver a greener, healthier and more liveable space. <span>Check out the interactive gallery below </span>using the sort filters to explore student projects!
+  	<span>
+    We partner with local post-secondary schools to spur student-driven projects that make Vancouver a greener, healthier and more liveable space. 	<br><b>Check out the interactive gallery below</b>
+    </span>
+    <span class="gallery-description">
+     using the sort filters to explore student projects!
+    </span>
   </p>
