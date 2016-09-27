62aa226b1b9d90babdc59d88c3bc28e19b7465b6
Adam Dunlop <adamblairdunlop@gmail.com>
2016-09-26 19:26:12 -0700

    updated the files

--- wp-content/themes/citystudio/js/sort-function.js
+++ wp-content/themes/citystudio/js/sort-function.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
 
   if ($('body').hasClass('single-project') || $('body').hasClass('archive') || $('body').hasClass('search') ) {
-    
-    var navDiv = $('.nav-div');
-
+    $('#sort_nav').remove();
+    $('.nav-div').addClass('notransition');
+    $('.nav-div').css("background-color", "#006496");
   }
 
   // queryFilter holds an empty object for the checked navigation values/properties
@@ -19,7 +19,13 @@ jQuery(document).ready(function($) {
 
   // Sort navigation is open on page load on mobile
 
-  $('#refresh').on('click', function() {
+  $('#sort_nav').click(function() {
+    $('.theme-labels').toggleClass('show-labels');
+    $('.part-labels').toggleClass('show-labels');
+    $('.year-labels').toggleClass('show-labels');
+  }); // close sort
+
+  $('#refresh').click(function() {
     checkedTheme = '';
     checkedPart = '';
     checkedYear = '';
@@ -30,79 +36,81 @@ jQuery(document).ready(function($) {
       year: ''
     }
 
-    $('.theme-labels').empty().append('<i class="fa fa-times" aria-hidden="true"></i>').hide();
-    $('.part-labels').empty().append('<i class="fa fa-times" aria-hidden="true"></i>').hide();
-    $('.year-labels').empty().append('<i class="fa fa-times" aria-hidden="true"></i>').hide();
+    $('.theme-labels').empty().hide();
+    $('.part-labels').empty().hide();
+    $('.year-labels').empty().hide();
 
-      reloadProjects();
+
+    reloadProjects();
 
   }); // close refresh
 
-  $('.theme-labels').on('click', function(){
+  $('.theme-labels').click(function(){
     $(this).empty().hide();
-    $(this, "label").append('<i class="fa fa-times" aria-hidden="true"></i>');
   });
 
-  $('.part-labels').on('click', function(){
+  $('.part-labels').click(function(){
     $(this).empty().hide();
-    $(this, "label").append('<i class="fa fa-times" aria-hidden="true"></i>');
   });
 
-  $('.year-labels').on('click', function(){
+  $('.year-labels').click(function(){
     $(this).empty().hide();
-    $(this, "label").append('<i class="fa fa-times" aria-hidden="true"></i>');
   });
 
-  $('.themes').on('click', function(){
+  $('.themes.sort-menu-item').click(function(){
     $(this).toggleClass('toggle-menu-item');
     $(this).children().toggleClass('toggle-menu-item');
     $("i", this).toggleClass("fa-sort-desc fa-sort-asc");
   });
 
-   $('.partners').on('click', function(){
+   $('.partners.sort-menu-item').click(function(){
       $(this).toggleClass('toggle-menu-item');
       $(this).children().toggleClass('toggle-menu-item');
       $("i", this).toggleClass("fa-sort-desc fa-sort-asc");
   });
 
-  $('.years').on('click', function(){
+  $('.years.sort-menu-item').click(function(){
       $(this).toggleClass('toggle-menu-item');
       $(this).children().toggleClass('toggle-menu-item');
       $("i", this).toggleClass("fa-sort-desc fa-sort-asc");
-  }); 
+  });
+
+  $('.fa.fa-times').click(function(){
+    $('.theme-labels').empty().hide();
+    $('.part-labels').empty().hide();
+    $('.year-labels').empty().hide();
+
+    reloadProjects();
+  }) 
 
   // Remove class current-menu if mouse isn't hovering over menu-item
 
-  $('.themes').on('click', function(){
+  $('.themes').click(function(){
     $(this).children('.nav-sub-menu').children().addClass('current-menu');
-  });
-
-  $('#refresh.refresh.menu-item').hover(function(){
-      $(".refresh-hover").css({"opacity": 1});
-  }, function(){
-      $(".refresh-hover").css({"opacity": 0});
-    } 
-  );
+  })
 
   // Create an empty object to hold
   // the checked navigation values in the properties
-  $('.sub-menu-theme').on('click', function() {
+  $('.sub-menu-theme').click(function() {
+
     if ($(this.checked)) {
       queryFilter.themes = $(this).find('input').val();
       checkedTheme = ($(this).text()).trim();
-      debugger;
+            debugger;
+
       $('.theme-labels')
           .show()
           .append('<label>' + checkedTheme +'</label>');
       $(this).parent().removeClass('current-menu');
-      debugger;
       reloadProjects();
     }
   });
-  $('.sub-menu-part').on('click', function() {
+
+  $('.sub-menu-part').click(function() {
     if ($(this.checked)) {
       queryFilter.partners = $(this).find('input').val();
       checkedPart = ($(this).text()).trim();
+            debugger;
 
       $('.part-labels')
           .show()
@@ -112,7 +120,7 @@ jQuery(document).ready(function($) {
       reloadProjects();
     }
   });
-  $('.sub-menu-year').on('click', function() {
+  $('.sub-menu-year').click(function() {
     if ($(this.checked)) {
       queryFilter.year = $(this).find('input').val();
       checkedYear = parseInt(($(this).text()).trim());
@@ -144,9 +152,9 @@ jQuery(document).ready(function($) {
           url: api_vars.rest_url+'wp/v2/project?'+filters(),
           success: function(response, data, status) {
             var projects = response;
-            // debugger
             // create gallery method to append HTML to
             var $gallery = $('.grid');
+            debugger;
             var galleryItems = '';
               // Clear the Gallery after each sort data is added to repopulate the Gallery
               $gallery.empty();
@@ -156,7 +164,7 @@ jQuery(document).ready(function($) {
                 $.each(projects, function(index, value) {
                     galleryItems +=   '<a class="gallery-anchor" ';
                     galleryItems +=   'href="' + value.link + '">';
-                    galleryItems +=   '<li class="gallery-image-wrap" style="background: url(' + value.featured_image_url + ');">';
+                    galleryItems +=   '<li class="gallery-image" style="background: url(' + value.featured_image_url + ');">';
                     galleryItems +=   '<div class="description">';
                     galleryItems +=     '<h2 class="description-title">' + value.title.rendered + '</h2>';
                     galleryItems +=     '<div class="subtitle"> ' + value.subtitle + ' </div>';
@@ -164,6 +172,8 @@ jQuery(document).ready(function($) {
                     galleryItems +=   '</div>';
                     galleryItems +=   '</li>';
                     galleryItems += '</a>';
+                                debugger;
+
                 });
                   $gallery.append(galleryItems);
               }
