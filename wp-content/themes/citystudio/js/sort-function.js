acf7c3982ceb2461d6224989f296e630c03452ee
Adam Dunlop <adamblairdunlop@gmail.com>
2016-10-03 18:14:40 -0700

    update wp_url

--- wp-content/themes/citystudio/js/sort-function.js
+++ wp-content/themes/citystudio/js/sort-function.js
@@ -140,7 +140,7 @@ jQuery(document).ready(function($) {
       $.ajax({
           type: 'GET',
           dataType: 'json',
-          url: api_vars.rest_url+'wp/v2/project?'+filters()+'&[posts_per_page]=16',
+          url: api_vars.rest_url+'wp/v2/project?'+filters()+'&filter[posts_per_page]=16',
           success: function(response, data, status) {
             var projects = response;
             console.log("response", response);
