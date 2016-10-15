a22672cdaec7a1211dc70d1c6d01f452cbf3e9b3
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-10-15 11:41:51 -0700

    blog page

--- wp-content/themes/citystudio/home.php
+++ wp-content/themes/citystudio/home.php
@@ -8,10 +8,12 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 <!-- "info main top section -->
 <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-      <header class="title-banner blog-banner" style="background-image:  linear-gradient(to right, rgba(2,119,167,0.7) 0%,rgba(203,173,201,0.7) 100%), url( '<?php echo $thumb['0'];?> ' ); ">
-				<div class="title-container">
-	        <h1>CityStudio Blog</h1>
-          <h4><span>Keep your finger on the pulse.</span> CityStudio news, upcoming opportunities to get involved and find out what we are up to.</h4>
+      <header class="title-banner blog-banner" style="background-image: url( <?php echo $thumb['0'];?> ); ">
+				<div class="gradient-overlay">
+					<div class="title-container">
+		        <h1>CityStudio Blog</h1>
+	          <h4><span>Keep your finger on the pulse.</span> CityStudio news, upcoming opportunities to get involved and find out what we are up to.</h4>
+					</div>
 				</div>
       </header>
       <div class="blog-content-container">
@@ -41,17 +43,22 @@ get_header(); ?>
 
         <div class="blog-archive-feed">
 					<?php
+						$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 					  $args = array(
 					  'post_type' => 'post',
+						'posts_per_page' => 3,
+ 						'paged' => $paged,
+						'orderby' => 'date',
+						'page_slug' => 'blog',
+ 						'post__not_in' => array('project','people','Partners')
 					);
 					$citystudio_blog = new WP_Query( $args ); ?>
 
 					<?php if (   $citystudio_blog->have_posts() ) : ?>
 
 					<?php /* Start the Loop */ ?>
+				  <?php while ( $citystudio_blog->have_posts() ) : $citystudio_blog->the_post(); ?>
 
-
-					  <?php while ( $citystudio_blog->have_posts() ) : $citystudio_blog->the_post(); ?>
 						<?php $blogfeatureimage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 							<a href= '<?php echo get_permalink( $post->ID ); ?>'>
 							<li class="blog-featured-image" style="background: url('<?php echo $blogfeatureimage['0'];?>') no-repeat center;
@@ -63,13 +70,20 @@ get_header(); ?>
 									</h2>
 									<p><?php the_field('post_excerpt'); ?></p>
 									<div class="category">
-										<!-- <?php the_category(', '); ?> -->
+										<?php the_category(', '); ?>
 									</div>
 								</div>
 							</li>
 							</a>
 					      <?php endwhile; ?>
-					<?php else : ?>
+
+					<div class="blog-pagination">
+						<?php echo paginate_links( $args ); ?>
+	  			</div>
+
+					<?php wp_reset_postdata(); ?>
+					<?php else:  ?>
+					  <p><?php _e( 'Sorry, no posts matched your criteria. You have reached the end of the blog! For more updates, check out our Twitter, Facebook, and other social media feeds!' ); ?></p>
 
 					<?php endif; ?>
 
