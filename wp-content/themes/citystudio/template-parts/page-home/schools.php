66892a2eb6cc878319a7320bac0bc596f980300a
Emilie <emilie@goatsocial.com>
2016-10-15 13:30:01 -0700

    fix partners links

--- wp-content/themes/citystudio/template-parts/page-home/schools.php
+++ wp-content/themes/citystudio/template-parts/page-home/schools.php
@@ -1,32 +1,34 @@
 
 <h1 class="homepage-titles">Partners</h1>
 
-  <div class="homepage-partners-container">
-    <ul class="featured">
+<div class="homepage-partners-container">
+    <ul class="home-partners">
       <?php
         if( have_rows('partner_row') );
         while ( have_rows('partner_row') ) : the_row(); ?>
-        <a href="<?php the_sub_field('partner_link'); ?>"><li style="background: url( '<?php the_sub_field('school_logo'); ?> ' ) no-repeat center center; background-size: contain; ">
-        </li></a>
+        <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
+          <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo'); ?>' )"></li>
+        </a>
       <?php endwhile; ?>
     </ul>
 
-    <ul class="featured-two">
+    <ul class="home-partners">
         <?php
           if( have_rows('partner_row_two') );
           while ( have_rows('partner_row_two') ) : the_row(); ?>
-          <a href="<?php the_sub_field('partner_link'); ?>"><li style="background: url( ' <?php the_sub_field('school_logo') ?> ' )no-repeat center center; background-size: contain; ">
-          </li></a>
+          <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
+            <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo') ?>' )"></li>
+          </a>
         <?php endwhile; ?>
     </ul>
 
-    <ul class="featured-two">
+    <ul class="home-partners">
         <?php
           if( have_rows('partner_row_three') );
           while ( have_rows('partner_row_three') ) : the_row(); ?>
-          <a href="<?php the_sub_field('partner_link'); ?>"><li style="background: url( ' <?php the_sub_field('school_logo') ?> ' )no-repeat center center; background-size: contain; ">
-          </li></a>
+          <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
+            <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo') ?>' ) "></li>
+          </a>
         <?php endwhile; ?>
     </ul>
-
-  </div>
+</div>
