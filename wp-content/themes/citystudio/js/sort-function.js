340e554387d4eebb6d3b8afe0714615878cb833b
Adam Dunlop <adamblairdunlop@gmail.com>
2016-10-21 14:21:04 -0700

    updated styles

--- wp-content/themes/citystudio/js/sort-function.js
+++ wp-content/themes/citystudio/js/sort-function.js
@@ -133,8 +133,8 @@ jQuery(document).ready(function($) {
       $.ajax({
           type: 'GET',
           dataType: 'json',
-          url: api_vars.rest_url+'wp/v2/project?'+filters()+'&filter[posts_per_page]=16',
-          // url: '-2016/wp-json/wp/v2/project?'+filters()+'&filter[posts_per_page]=16',
+          // url: api_vars.rest_url+'wp/v2/project?'+filters()+'&filter[posts_per_page]=16',
+          url: 'http://localhost:3000/citystudio-winter-2016/wp-json/wp/v2/project?'+filters()+'&filter[posts_per_page]=16',
             success: function(response, data, status) {
             var projects = response;
             console.log("response", response);
