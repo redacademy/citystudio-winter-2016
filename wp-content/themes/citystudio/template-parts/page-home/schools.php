6b7f46a519d2af20789318225a717e079ff7bb61
Riva Adara Ridley <rivaadara@gmail.com>
2016-11-27 15:47:55 -0800

    remove sort bar stick and 3rd partner logo row

--- wp-content/themes/citystudio/template-parts/page-home/schools.php
+++ wp-content/themes/citystudio/template-parts/page-home/schools.php
@@ -21,13 +21,4 @@
         <?php endwhile; ?>
     </ul>
 
-    <!-- <?php
-    if( have_rows('partner_row_three') );?>
-    <ul class="home-partners">
-      <?php  while ( have_rows('partner_row_three') ) : the_row(); ?>
-        <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
-          <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo') ?>' ) "></li>
-        </a>
-      <?php endwhile; ?>
-    </ul> -->
 </div>
