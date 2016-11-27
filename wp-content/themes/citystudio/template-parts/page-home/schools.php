c05499a34e2d924aa96b6bb00558abec4b867151
Emilie <emilie.nagahama@gmaill.com>
2016-11-27 11:44:36 -0800

    sort homepage descriptions

--- wp-content/themes/citystudio/template-parts/page-home/schools.php
+++ wp-content/themes/citystudio/template-parts/page-home/schools.php
@@ -1,33 +1,33 @@
 
 <div class="homepage-partners-container">
   <h1 class="homepage-titles"><?php the_field('partners_title'); ?></h1>
+    <?php
+      if( have_rows('partner_row') ); ?>
+        <ul class="home-partners">
+          <?php while ( have_rows('partner_row') ) : the_row(); ?>
+            <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
+              <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo'); ?>' )"></li>
+            </a>
+          <?php endwhile; ?>
+        </ul>
+
+    <?php
+    if( have_rows('partner_row_two') ); ?>
     <ul class="home-partners">
-      <?php
-        if( have_rows('partner_row') );
-        while ( have_rows('partner_row') ) : the_row(); ?>
+      <?php while ( have_rows('partner_row_two') ) : the_row(); ?>
         <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
-          <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo'); ?>' )"></li>
+          <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo') ?>' )"></li>
+        </a>
+        <?php endwhile; ?>
+    </ul>
+
+    <?php
+    if( have_rows('partner_row_three') );?>
+    <ul class="home-partners">
+      <?php  while ( have_rows('partner_row_three') ) : the_row(); ?>
+        <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
+          <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo') ?>' ) "></li>
         </a>
       <?php endwhile; ?>
-    </ul>
-
-    <ul class="home-partners">
-        <?php
-          if( have_rows('partner_row_two') );
-          while ( have_rows('partner_row_two') ) : the_row(); ?>
-          <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
-            <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo') ?>' )"></li>
-          </a>
-        <?php endwhile; ?>
-    </ul>
-
-    <ul class="home-partners">
-        <?php
-          if( have_rows('partner_row_three') );
-          while ( have_rows('partner_row_three') ) : the_row(); ?>
-          <a class="feat-logo-link" href="<?php the_sub_field('partner_link'); ?>">
-            <li class="feat-logo" style="background-image: url( '<?php the_sub_field('school_logo') ?>' ) "></li>
-          </a>
-        <?php endwhile; ?>
     </ul>
 </div>
